f0ba63b99a6476a2c1470baf2893e36fecae4365
David-santhan <156925284+David-santhan@users.noreply.github.com>
2024-09-13 12:09:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -67,6 +67,20 @@ const corsOptions = {
   };
   
   app.use(cors(corsOptions));
+
+const allowedOrigins = ['http://localhost:3000', 'https://frontend-theta-mocha-38.vercel.app'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
  
   
 app.use("/www", express.static("uploads"));
