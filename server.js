089c7cf024a6b4ed984e2e8e196a878450537bb6
David-santhan <156925284+David-santhan@users.noreply.github.com>
2024-09-13 17:32:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -89,13 +89,9 @@ app.use(cors({
 }));
 app.options('*', cors());  // Preflight requests
 
-res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');  // Use requested origin
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', 'https://frontend-theta-mocha-38.vercel.app');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
